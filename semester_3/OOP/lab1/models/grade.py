dae18b0c9c30fe40820479245e6308b5700ef2fb
Liashenko Matvii <105646361+khaenare@users.noreply.github.com>
2024-12-05 16:51:59 +0200

    Update grade.py

--- semester_3/OOP/lab1/models/grade.py
+++ semester_3/OOP/lab1/models/grade.py
@@ -1,6 +1,12 @@
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from models.student import Student
+    from models.assignment import Assignment
+
 class Grade:
     """
-    Класс, представляющий оценку за задание.
+    Клас, що представляє оцінку за завдання.
     """
 
     def __init__(self, student: 'Student', assignment: 'Assignment', grade_value: float):
@@ -10,11 +16,7 @@ class Grade:
 
     def update_grade(self, new_value: float):
         """
-        Обновляет значение оценки.
+        Оновлює значення оцінки.
         """
         self.grade_value = new_value
-        print(f"Оценка обновлена до {new_value} для студента {self.student.get_full_name()} по заданию {self.assignment.title}")
-
-# Избегаем циклического импорта
-from models.student import Student
-from models.assignment import Assignment
+        print(f"Grade updated to {new_value} for {self.student.get_full_name()} on assignment {self.assignment.title}")
