be2deeb04cdea62e5d6e79b58f868ca408c5dd4a
Liashenko Matvii <105646361+khaenare@users.noreply.github.com>
2024-12-05 16:50:48 +0200

    Update onsite_course.py

--- semester_3/OOP/lab1/models/onsite_course.py
+++ semester_3/OOP/lab1/models/onsite_course.py
@@ -2,7 +2,7 @@ from models.course import Course
 
 class OnsiteCourse(Course):
     """
-    Класс, представляющий очный курс.
+    Клас, що представляє очний курс.
     """
 
     def __init__(self, course_id: str, title: str, description: str, location: str):
@@ -11,6 +11,6 @@ class OnsiteCourse(Course):
 
     def get_course_info(self) -> str:
         """
-        Возвращает информацию об очном курсе.
+        Повертає інформацію про очний курс.
         """
-        return f"{self.title} проводится в {self.location}"
+        return f"{self.title} is held at {self.location}"
