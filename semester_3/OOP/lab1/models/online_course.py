1e2d71b0f346ffd7fa48ef85d404e2064f4c3f23
Liashenko Matvii <105646361+khaenare@users.noreply.github.com>
2024-12-05 16:50:23 +0200

    Update online_course.py

--- semester_3/OOP/lab1/models/online_course.py
+++ semester_3/OOP/lab1/models/online_course.py
@@ -2,7 +2,7 @@ from models.course import Course
 
 class OnlineCourse(Course):
     """
-    Класс, представляющий онлайн-курс.
+    Клас, що представляє онлайн-курс.
     """
 
     def __init__(self, course_id: str, title: str, description: str, platform: str):
@@ -11,6 +11,6 @@ class OnlineCourse(Course):
 
     def get_course_info(self) -> str:
         """
-        Возвращает информацию об онлайн-курсе.
+        Повертає інформацію про онлайн-курс.
         """
-        return f"{self.title} доступен на платформе {self.platform}"
+        return f"{self.title} is available on {self.platform}"
