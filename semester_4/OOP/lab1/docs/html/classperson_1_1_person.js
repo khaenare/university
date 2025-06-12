b319e3605491444a6cc35b06a9902934897ab2aa
Liashenko Matvii <105646361+khaenare@users.noreply.github.com>
2025-06-12 20:20:34 +0300

    Add files via upload

---
+++ semester_4/OOP/lab1/docs/html/classperson_1_1_person.js
@@ -0,0 +1,20 @@
+var classperson_1_1_person =
+[
+    [ "__init__", "classperson_1_1_person.html#a03563026be5cd914b2a4e3d5d8eade7f", null ],
+    [ "__str__", "classperson_1_1_person.html#a44eae0686791b4b64d27d866ee1a2c8b", null ],
+    [ "email", "classperson_1_1_person.html#a021f0f921160a53a160dfb79a52e47f9", null ],
+    [ "email", "classperson_1_1_person.html#acd39cbd36af592add6cc72967310108d", null ],
+    [ "get_email", "classperson_1_1_person.html#ab4aba6d7d142d49260001e77bfcff46e", null ],
+    [ "get_full_name", "classperson_1_1_person.html#ada844d767d40b17dad265aa9ae391606", null ],
+    [ "name", "classperson_1_1_person.html#ae9933c49275e1f37d531511cbd2ba79f", null ],
+    [ "name", "classperson_1_1_person.html#a888d4083ca3052668347ac23069b0c36", null ],
+    [ "set_email", "classperson_1_1_person.html#ae22ad1c3d14ff9ae5034ecc870097c93", null ],
+    [ "surname", "classperson_1_1_person.html#ae58dc5bd54763889a35c0846b1c73ed4", null ],
+    [ "surname", "classperson_1_1_person.html#a0841addc31a937ed87c3b8e014ddcbd2", null ],
+    [ "_email", "classperson_1_1_person.html#af886c52a88077b852ad85f1f687d645b", null ],
+    [ "_name", "classperson_1_1_person.html#a10d5a45a928c7ad6548d4ed479163807", null ],
+    [ "_surname", "classperson_1_1_person.html#a4fae7466fd95cec07fd2bdd59ddff1e5", null ],
+    [ "email", "classperson_1_1_person.html#a8b25bf97c03aca090269c8af65ed7815", null ],
+    [ "name", "classperson_1_1_person.html#ad362c03d613dba05ebf52aeb67773ebc", null ],
+    [ "surname", "classperson_1_1_person.html#a44db1bb34273894fc1664e62bdbd2d4f", null ]
+];
---
+++ semester_4/OOP/lab1/docs/html/namespaces_dup.js
@@ -0,0 +1,19 @@
+var namespaces_dup =
+[
+    [ "administrator", "namespaceadministrator.html", "namespaceadministrator" ],
+    [ "assignment", "namespaceassignment.html", "namespaceassignment" ],
+    [ "course", "namespacecourse.html", "namespacecourse" ],
+    [ "grade", "namespacegrade.html", "namespacegrade" ],
+    [ "main", "namespacemain.html", [
+      [ "demo", "namespacemain.html#a6baff27adc141e720841caf845eddc65", null ],
+      [ "main", "namespacemain.html#aa7a58ac748c5293faa4d77a82b94d161", null ],
+      [ "choice", "namespacemain.html#a64aa1885eb5b796c96a96cc4f3c7289a", null ]
+    ] ],
+    [ "online_course", "namespaceonline__course.html", "namespaceonline__course" ],
+    [ "onsite_course", "namespaceonsite__course.html", "namespaceonsite__course" ],
+    [ "person", "namespaceperson.html", "namespaceperson" ],
+    [ "schedule", "namespaceschedule.html", "namespaceschedule" ],
+    [ "student", "namespacestudent.html", "namespacestudent" ],
+    [ "system_interface", "namespacesystem__interface.html", "namespacesystem__interface" ],
+    [ "teacher", "namespaceteacher.html", "namespaceteacher" ]
+];
