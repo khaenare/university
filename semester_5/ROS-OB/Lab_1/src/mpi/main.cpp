1a4653f466b96ea6628d0a8b588858f52d32c943
khaenare <lmatviineko@gmail.com>
2025-10-05 20:18:35 +0300

    Exercise 4: Task 1 — create minimal project skeleton for ParallelMatrixVectorMult

---
+++ semester_5/ROS-OB/Lab_1/src/mpi/main.cpp
@@ -0,0 +1,9 @@
+// src/parallel/main.cpp
+// Exercise 4 — Task 1: open the project ParallelMatrixVectorMult (C++17)
+
+#include <iostream>
+
+int main() {
+    std::cout << "Parallel matrix-vector multiplication program" << std::endl;
+    return 0;
+}
