fe7e33b15fcd8d8beb1e3c95537eb2b1aefc7427
khaenare <lmatviineko@gmail.com>
2025-11-17 20:48:38 +0200

    2

--- semester_5/mcs/lab2/test.py
+++ semester_5/mcs/lab2/test.py
@@ -1,6 +1,15 @@
 from image_io import read_grayscale_image
+from utils import augment_with_ones
 
+# Зчитуємо вхідне зображення
 X = read_grayscale_image("x3.bmp")
-Y = read_grayscale_image("y8.bmp")
 
-print(X.shape, Y.shape, X.min(), X.max(), Y.min(), Y.max())
+# Формуємо розширену матрицю
+X_tilde = augment_with_ones(X)
+
+print("Стара форма X:", X.shape)
+print("Нова форма  X̃:", X_tilde.shape)
+
+# Перевірка, що останній рядок — одиниці
+print("Унікальні значення останнього рядка:",
+      set(X_tilde[-1].round(5)))
